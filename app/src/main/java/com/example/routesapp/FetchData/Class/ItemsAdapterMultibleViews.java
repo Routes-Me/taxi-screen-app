c09efefb7ead919498a2fa1d1b161918c57d301c
Abdullah Soubeih <AbdullahSoubeih@gmail.com>
2019-10-16 15:26:03 +0300

    16/10/2019

--- app/src/main/java/com/example/routesapp/FetchData/Class/ItemsAdapterMultibleViews.java
+++ app/src/main/java/com/example/routesapp/FetchData/Class/ItemsAdapterMultibleViews.java
@@ -32,7 +32,7 @@ public class ItemsAdapterMultibleViews extends RecyclerView.Adapter<RecyclerView
 
 
 
-    
+
     private static final int ITEM_QRCode = 1;
     private static final int ITEM_Map = 2;
     private static final int ITEM_Web = 3;
@@ -45,6 +45,7 @@ public class ItemsAdapterMultibleViews extends RecyclerView.Adapter<RecyclerView
     Context mCtx;
     List<ItemsModel> itemsList;
 
+    
 
     private OnItemClickListener listener;
 
