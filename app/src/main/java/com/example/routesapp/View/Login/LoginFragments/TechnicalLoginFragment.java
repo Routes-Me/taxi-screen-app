9a18a098d4f8477481025edcb77cafc55cff382a
Abdullah Soubeih <AbdullahSoubeih@gmail.com>
2019-11-24 12:33:48 +0300

    16/10/2019

--- app/src/main/java/com/example/routesapp/View/Login/LoginFragments/TechnicalLoginFragment.java
+++ app/src/main/java/com/example/routesapp/View/Login/LoginFragments/TechnicalLoginFragment.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.routesapp.R;
 import com.example.routesapp.View.Login.Activity.LearnMoreScreen;
+import com.scottyab.showhidepasswordedittext.ShowHidePasswordEditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +30,9 @@ public class TechnicalLoginFragment extends Fragment implements View.OnClickList
 
     private TextView btn_next;
     private LinearLayout btn_learnMore;
+
+    private EditText email_et, password_et;
+    private TextView email_error_tv, password_error_tv;
 
     public TechnicalLoginFragment() {
         // Required empty public constructor
@@ -47,6 +55,33 @@ public class TechnicalLoginFragment extends Fragment implements View.OnClickList
         btn_next.setOnClickListener(this);
         btn_learnMore = nMainView.findViewById(R.id.btn_learnMore);
         btn_learnMore.setOnClickListener(this);
+
+        email_et = nMainView.findViewById(R.id.email_et);
+        email_error_tv = nMainView.findViewById(R.id.email_error_tv);
+        password_et = nMainView.findViewById(R.id.password_et);
+        password_error_tv = nMainView.findViewById(R.id.password_error_tv);
+        email_et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showErrorMessage(email_et, email_error_tv,"",false);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+        password_et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showErrorMessage(password_et, password_error_tv,"",false);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
     }
 
     @Override
@@ -66,5 +101,65 @@ public class TechnicalLoginFragment extends Fragment implements View.OnClickList
     }
 
     private void openTabletDataFragment() {
-        getActivity().getSupportFragmentManager().beginTransaction().setCustomAnimations( R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right).replace(R.id.login_fragment_container, new TabletDataFragment()).commit();    }
+
+
+        String email = email_et.getText().toString().trim();
+        String password = password_et.getText().toString().trim();
+
+
+
+        if (email.isEmpty()){
+           // email_et.setError("Email Address Required");
+          //  email_et.requestFocus();
+
+            showErrorMessage(email_et, email_error_tv,"* Email Address Required",true);
+
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+          //  email_et.setError("Enter Valid Email");
+          //  email_et.requestFocus();
+            showErrorMessage(email_et, email_error_tv,"* Enter Valid Email",true);
+            return;
+        }
+
+        if (password.isEmpty()){
+           // password_et.setError("Password Required");
+           // password_et.requestFocus();
+
+            showErrorMessage(password_et, password_error_tv,"* Password Required",true);
+
+            return;
+        }
+
+        if (password.length() < 8){
+          //  password_et.setError("Minimum Password is 8 digit");
+          //  password_et.requestFocus();
+
+            showErrorMessage(password_et, password_error_tv,"* Minimum Password is 8 digit",true);
+
+            return;
+        }
+
+
+        getActivity().getSupportFragmentManager().beginTransaction().setCustomAnimations( R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right).replace(R.id.login_fragment_container, new TabletDataFragment()).commit();
+    }
+
+
+    private void showErrorMessage(EditText editText,TextView errorTv, String errorStr, boolean show){
+
+        if (show){
+            editText.setBackgroundResource(R.drawable.red_border);
+            errorTv.setText(errorStr);
+            errorTv.setVisibility(View.VISIBLE);
+        }else {
+            editText.setBackgroundResource(R.drawable.grey_border);
+            errorTv.setVisibility(View.INVISIBLE);
+        }
+
+
+
+    }
+
+
 }
