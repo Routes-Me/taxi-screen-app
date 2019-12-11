e17fba3a0c8c20f00541d957f92b22b0d53f3fe1
Abdullah Soubeih <AbdullahSoubeih@gmail.com>
2019-12-11 16:35:41 +0300

    Completing design of taxi information screen

--- app/src/main/java/com/example/routesapp/View/Login/Activity/TaxiInformationScreen.java
+++ app/src/main/java/com/example/routesapp/View/Login/Activity/TaxiInformationScreen.java
@@ -1,18 +1,29 @@
 package com.example.routesapp.View.Login.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.routesapp.Class.App;
 import com.example.routesapp.R;
-import com.example.routesapp.View.Activity.MainActivity;
 
-public class TaxiInformationScreen extends AppCompatActivity {
+public class TaxiInformationScreen extends AppCompatActivity implements View.OnClickListener {
 
     private App app;
+
+    private Toolbar myToolbar;
+
+    private TelephonyManager telephonyManager;
+
+    private TextView deviceSerialNumber_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +32,102 @@ public class TaxiInformationScreen extends AppCompatActivity {
 
 
         app = (App) getApplicationContext();
-        Toast.makeText(this, "userName:  " + app.getTechnicalSupportName(), Toast.LENGTH_SHORT).show();
+        app.setTechnicalSupportName("Abdullah Soubeih");
+
+        ToolbarSetUp();
+
+
 
      //   startActivity(new Intent(this, MainActivity.class));
        // finish();
+
+
+        deviceSerialNumber_et = findViewById(R.id.deviceSerialNumber_et);
+        deviceSerialNumber_et.setOnClickListener(this);
+
+        getDeviceId();
+
+    }
+
+
+    private void ToolbarSetUp() {
+        //Toolbar..
+        myToolbar = findViewById(R.id.MyToolBar);
+
+
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setTitle("Welcome,  " + app.getTechnicalSupportName());
+
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_grey);
+        }
+
+    }
+
+
+    private void getDeviceId() {
+        try {
+            telephonyManager = (TelephonyManager) getSystemService(this.TELEPHONY_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 101);
+                return;
+            }else {
+                //Toast.makeText(TaxiInformationScreen.this,telephonyManager.getDeviceId(),Toast.LENGTH_LONG).show();
+
+                deviceSerialNumber_et.setText(telephonyManager.getDeviceId());
+                deviceSerialNumber_et.setError(null);//removes error
+                deviceSerialNumber_et.clearFocus();
+            }
+        }catch (Exception e){
+         //   Toast.makeText(this, "Device ID Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 101);
+                        return;
+                    }
+                   // Toast.makeText(TaxiInformationScreen.this,telephonyManager.getDeviceId(),Toast.LENGTH_LONG).show();
+                    deviceSerialNumber_et.setText(telephonyManager.getDeviceId());
+                    deviceSerialNumber_et.setError(null);//removes error
+                    deviceSerialNumber_et.clearFocus();
+                } else {
+                   // Toast.makeText(TaxiInformationScreen.this,"Without permission we check",Toast.LENGTH_LONG).show();
+
+                  //  if (Link.isEmpty()){
+                    deviceSerialNumber_et.setError("Click here to get serial number");
+                    deviceSerialNumber_et.requestFocus();
+                        return;
+                  //  }
+
+                   // deviceSerialNumber_et.setText("Get Tablet Serial Number");
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.deviceSerialNumber_et:
+                    getDeviceId();
+                break;
+
+        }
 
     }
 }
