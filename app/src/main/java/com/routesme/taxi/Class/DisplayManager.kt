ea6f88e9c7b0ec8eb1aa19175c4b1a03b7adf15b
Abdullah Soubeih <asoubeih@routesme.com>
2020-11-08 14:28:41 +0300

    Change the alarms of light mode to (5AM) and dark mode to (5PM)

--- app/src/main/java/com/routesme/taxi/Class/DisplayManager.kt
+++ app/src/main/java/com/routesme/taxi/Class/DisplayManager.kt
@@ -24,12 +24,12 @@ open class DisplayManager() {
 
     fun setAlarm(context: Context) {
         val cal1 = Calendar.getInstance()
-        cal1[Calendar.HOUR_OF_DAY] = 6
+        cal1[Calendar.HOUR_OF_DAY] = 5
         cal1[Calendar.MINUTE] = 1
         cal1[Calendar.SECOND] = 0
 
         val cal2 = Calendar.getInstance()
-        cal2[Calendar.HOUR_OF_DAY] = 18
+        cal2[Calendar.HOUR_OF_DAY] = 17
         cal2[Calendar.MINUTE] = 1
         cal2[Calendar.SECOND] = 0
 
@@ -46,7 +46,7 @@ open class DisplayManager() {
         alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, cal2.timeInMillis, eveningAlarm)
     }
 
-    fun isAnteMeridiem() = currentDate().after(parseDate("06:00")) && currentDate().before(parseDate("18:00"))
+    fun isAnteMeridiem() = currentDate().after(parseDate("05:00")) && currentDate().before(parseDate("17:00"))
     private fun currentDate(): Date {
         val calendar = Calendar.getInstance()
         val hour = calendar.get(Calendar.HOUR_OF_DAY)
