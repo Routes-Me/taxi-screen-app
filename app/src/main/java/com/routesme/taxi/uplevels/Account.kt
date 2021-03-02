c4a0b8c0a040d45ac993443ce9437e6da0a6eecd
Abdullah Soubeih <asoubeih@routesme.com>
2021-03-02 15:26:55 +0300

    Remove testing text from access token that added to test unauthorized 401 token.

--- app/src/main/java/com/routesme/taxi/uplevels/Account.kt
+++ app/src/main/java/com/routesme/taxi/uplevels/Account.kt
@@ -8,7 +8,7 @@ class Account {
     var accessToken: String?
         get() {
             sharedPrefs().getString(SharedPreferencesHelper.token, null)?.let{
-                return "Bearer $it dfj"
+                return "Bearer $it"
             }
             return null
         }
