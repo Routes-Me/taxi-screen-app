6ea4675719343796171046f2814c67b46a857261
Abdullah Soubeih <asoubeih@routesme.com>
2021-03-09 11:29:19 +0300

    Add cookies missing files

---
+++ app/src/main/java/com/routesme/taxi/api/interceptors/BasicInterceptor.kt
@@ -0,0 +1,74 @@
+package com.routesme.taxi.api.interceptors
+
+import android.annotation.TargetApi
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
+import com.routesme.taxi.BuildConfig
+import com.routesme.taxi.R
+import com.routesme.taxi.helper.AdminConsoleHelper
+import com.routesme.taxi.helper.Helper
+import com.routesme.taxi.uplevels.Account
+import com.routesme.taxi.view.activity.LoginActivity
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+import java.net.HttpURLConnection
+
+internal class BasicAuthInterceptor(val context: Context) : Interceptor {
+
+    @TargetApi(Build.VERSION_CODES.N)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+                .addHeader(Header.Authorization.toString(), Account().accessToken.toString())
+                .addHeader(Header.CountryCode.toString(), countryCode())
+                .addHeader(Header.AppVersion.toString(), appVersion())
+                .addHeader(Header.application.toString(), "screen")
+                .build()
+        return chain.proceed(request)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun countryCode() = context.resources.configuration.locales.get(0).country
+
+    private fun appVersion() = "${BuildConfig.VERSION_NAME}.${BuildConfig.VERSION_CODE}"
+}
+
+internal class UnauthorizedInterceptor(val activity: Activity) : Interceptor {
+    private val AUTHORIZATION_KAY = "authorization"
+    private var adminConsoleHelper = AdminConsoleHelper(activity)
+    @Throws(IOException::class)
+    override fun intercept(@NonNull chain: Interceptor.Chain): Response {
+        val response: Response = chain.proceed(chain.request())
+        if (response.code() == HttpURLConnection.HTTP_UNAUTHORIZED) openModelPresenterScreen(activity, response.code())
+        return response
+    }
+
+    private fun openModelPresenterScreen(activity: Activity, responseCode: Int) {
+        adminConsoleHelper.logOff()
+    }
+}
+
+internal class NotAcceptableRefreshTokenInterceptor(val context: Context) : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(@NonNull chain: Interceptor.Chain): Response {
+        val baseUrl = Helper.getConfigValue("baseUrl", R.raw.config)!!
+        val response: Response = chain.proceed(chain.request())
+        val request = response.request()
+        if (response.code() == HttpURLConnection.HTTP_NOT_FOUND && request.url().toString() == baseUrl + "authentications/renewals") {
+            Log.d("RefreshToken", "${request.url()} not found !")
+
+          //  val currentActivity: Activity = (ApplicationProvider.getApplicationContext() as MyApp).getCurrentActivity()
+
+             context.startActivity(Intent(context, LoginActivity::class.java))
+             (context as Activity).finish()
+        }
+        return response
+    }
+}
+
+enum class Header { Authorization, CountryCode, AppVersion, application }
---
+++ app/src/main/java/com/routesme/taxi/api/interceptors/TokenRefreshAuthenticator.kt
@@ -0,0 +1,68 @@
+package com.routesme.taxi.api.interceptors
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import com.routesme.taxi.App
+import com.routesme.taxi.R
+import com.routesme.taxi.api.Constants
+import com.routesme.taxi.helper.Helper
+import com.routesme.taxi.view.activity.RefreshTokenActivity
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+
+class TokenRefreshAuthenticator(private val context: Context): Authenticator{
+    private val baseUrl = Helper.getConfigValue("baseUrl", R.raw.config)!!
+    override fun authenticate(route: Route?, response: Response): Request? = when {
+
+/*
+        response.networkResponse()?.request()?.url().toString() == baseUrl + "authentications/renewals" && response.code() == HttpURLConnection.HTTP_NOT_FOUND -> {
+            Log.d("RefreshToken", "Renewals request .. response code: ${response.code()}")
+            null
+        }
+
+        response.networkResponse()?.request()?.url().toString() == baseUrl + "authentications/renewals" && response.code() == HttpURLConnection.HTTP_NOT_ACCEPTABLE -> {
+           // logOutAuthenticator()
+            null
+        }
+        */
+
+        response.networkResponse()?.request()?.url().toString() == baseUrl + "authentications/renewals" -> null
+        response.networkResponse()?.request()?.url().toString() == baseUrl + "authentications" -> null
+        //retryCount(response.request()) == 1 -> null
+        else -> {
+            if (!App.instance.isRefreshActivityAlive) {
+                App.instance.isRefreshActivityAlive = true
+                openRefreshTokenActivity()
+            }
+            null
+        }//response.createSignedRequest()
+    }
+
+    private fun openRefreshTokenActivity() {
+        context.startActivity(Intent(context, RefreshTokenActivity::class.java))
+        (context as Activity).finish()
+    }
+
+    private fun retryCount(request: Request?)= request?.header(Constants.httpHeaderRetryCount)?.toInt() ?: 0
+/*
+    private fun Response.createSignedRequest(): Request? {
+        val refreshTokenResponse = TokenRepository(context).refreshToken()
+        val accessToken = refreshTokenResponse.value?.accessToken
+        val refreshToken = refreshTokenResponse.value?.refreshToken
+        accessToken?.let { Account().accessToken = it}
+        refreshToken?.let { Account().refreshToken = it }
+        return request().signWithToken()
+    }
+    private fun Request.signWithToken(): Request {
+  //  Log.d("Retry-Count", "Request: ${this.url()}, Count times: ${retryCount(this)}")
+   return newBuilder()
+            .header(Constants.httpHeaderRetryCount, "${retryCount(this) + 1}")
+            .removeHeader(Header.Authorization.toString())
+            .addHeader(Header.Authorization.toString(), Account().accessToken.toString())
+            .build()
+}
+    */
+}
