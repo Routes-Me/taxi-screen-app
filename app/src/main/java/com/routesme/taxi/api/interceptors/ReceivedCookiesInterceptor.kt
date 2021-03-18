df4e3f859be11d3024864268d70eeeef400f8cb7
Abdullah Soubeih <asoubeih@routesme.com>
2021-03-18 13:43:15 +0300

    Handle authentications cookies and get the refresh token.

--- app/src/main/java/com/routesme/taxi/api/interceptors/ReceivedCookiesInterceptor.kt
+++ app/src/main/java/com/routesme/taxi/api/interceptors/ReceivedCookiesInterceptor.kt
@@ -1,48 +1,19 @@
 package com.routesme.taxi.api.interceptors
 
-import android.content.Context
-import android.preference.PreferenceManager
+import com.routesme.taxi.uplevels.Account
 import okhttp3.Interceptor
-import okhttp3.Request
 import okhttp3.Response
-import java.io.IOException
+import java.net.HttpCookie
 
-class ReceivedCookiesInterceptor(private val context: Context) : Interceptor {
-    companion object {
-        const val PREF_COOKIES = "PREF_COOKIES"
-    }
-   // @Throws(IOException::class)
+class ReceivedCookiesInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val originalResponse = chain.proceed(chain.request())
-        if (originalResponse.headers("Set-Cookie").isNotEmpty()) {
-            val cookies = PreferenceManager.getDefaultSharedPreferences(context).getStringSet(PREF_COOKIES, HashSet()) as HashSet<String>
-            for (header in originalResponse.headers("Set-Cookie")) {
-                cookies.add(header)
-            }
-            val memes = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            memes.putStringSet(PREF_COOKIES, cookies).apply()
-            memes.commit()
+        val response = chain.proceed(chain.request())
+        val setCookie = response.headers("Set-Cookie")
+        if (!setCookie.isNullOrEmpty()) {
+            val cookieList: List<HttpCookie> = HttpCookie.parse(setCookie.first())
+            val refreshToken = cookieList.firstOrNull { it.name == "refreshToken" }?.value
+            refreshToken?.let { Account().refreshToken = it }
         }
-        return originalResponse
-    }
-}
-
-class AddCookiesInterceptor(private val context: Context) : Interceptor {
-
-    companion object {
-        const val PREF_COOKIES = "PREF_COOKIES"
-    }
-
-   // @Throws(IOException::class)
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val builder: Request.Builder = chain.request().newBuilder()
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context).getStringSet(PREF_COOKIES, HashSet()) as HashSet<String>
-        val original: Request = chain.request()
-        if (original.url().toString().contains("distributor")) {
-            for (cookie in preferences) {
-                builder.addHeader("Cookie", cookie)
-            }
-        }
-        return chain.proceed(builder.build())
+        return response
     }
 }
