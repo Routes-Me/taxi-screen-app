1e87f0a6c4fdf630f4f19fbd33d79235a519ed29
rohitdeveloper46 <rohit.developer46@yahoo.com>
2021-04-01 11:08:35 +0300

    Refine code

--- app/src/main/java/com/routesme/taxi/MVVM/Model/RegistrationModel.kt
+++ app/src/main/java/com/routesme/taxi/MVVM/Model/RegistrationModel.kt
@@ -1,16 +1,23 @@
-package com.routesme.taxi.MVVM.Model
+package com.routesme.taxi.data.model
 
-    data class RegistrationCredentials(var serialNumber: String? = null, var `SimSerialNumber`: String? = null, var VehicleId: String? = null)
+import com.routesme.taxi.data.model.Response
+import com.routesme.taxi.data.model.ResponseErrors
+
+data class RegistrationCredentials(var serialNumber: String? = null, var `SimSerialNumber`: String? = null, var VehicleId: String? = null)
 data class RegistrationSuccessResponse(val deviceId: String? = null, val status: Boolean = false, val message: String? = null, val statusCode: Int = -999)
 class RegistrationResponse(deviceId: String? = null, val mResponseErrors: ResponseErrors? = null, val mThrowable: Throwable? = null): Response(mResponseErrors, mThrowable) {
 
-     var deviceId: String? = null
+    var deviceId: String? = null
     init {
-       this.deviceId = deviceId
-       //this.deviceId = "15"
+       //this.deviceId = deviceId
+       this.deviceId = "3285684"
+
+
     }
 
     val isSuccess: Boolean
-        get() = (mResponseErrors == null && mThrowable == null)
-        //get() = true
+        //get() = (mResponseErrors == null && mThrowable == null)
+        get() = true
+
+
 }
