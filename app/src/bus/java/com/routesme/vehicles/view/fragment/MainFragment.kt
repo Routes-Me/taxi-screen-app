18b4082cd92192cf2562c0dfaf70e38edd1ff3c1
Abdullah Soubeih <asoubeih@routesme.com>
2021-04-14 14:44:20 +0300

    Refined main fragment code.

--- app/src/bus/java/com/routesme/vehicles/view/fragment/MainFragment.kt
+++ app/src/bus/java/com/routesme/vehicles/view/fragment/MainFragment.kt
@@ -6,11 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.routesme.vehicles.R
-import com.routesme.vehicles.helper.DateOperations
-import kotlinx.android.synthetic.bus.fragment_main.view.*
-import java.util.*
-import java.util.concurrent.TimeUnit
-import kotlin.concurrent.schedule
 
 class MainFragment : Fragment() {
     private lateinit var mainFragmentView: View
