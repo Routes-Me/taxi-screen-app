9d345faa6565868983bd88d15301b7be18815014
abdullahsoubeih <asoubeih@routesme.com>
2021-05-10 16:58:46 +0300

    Remove unused code from TicketsFragment.kt

--- app/src/bus/java/com/routesme/vehicles/view/fragment/TicketsFragment.kt
+++ app/src/bus/java/com/routesme/vehicles/view/fragment/TicketsFragment.kt
@@ -1,7 +1,6 @@
 package com.routesme.vehicles.view.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,6 @@ import com.routesme.vehicles.R
 import com.routesme.vehicles.uplevels.CarrierInformation
 import com.routesme.vehicles.view.adapter.PriceButtonRecyclerViewAdapter
 import kotlinx.android.synthetic.bus.fragment_tickets.view.*
-
 
 class TicketsFragment : Fragment(), PriceButtonRecyclerViewAdapter.ItemClickListener {
     private lateinit var selectTicketFragmentView: View
@@ -36,7 +34,6 @@ class TicketsFragment : Fragment(), PriceButtonRecyclerViewAdapter.ItemClickList
     }
 
     private fun setupPricesButtons() {
-
         val tickets = CarrierInformation().tickets
         tickets?.let { tickets ->
             val snapHelper = LinearSnapHelper()
@@ -44,45 +41,18 @@ class TicketsFragment : Fragment(), PriceButtonRecyclerViewAdapter.ItemClickList
             ticketsAdapter.setClickListener(this)
             val ticketSize = tickets.size
             val layoutManager= GridLayoutManager(activity,2)
-
-            Log.d("ticketsGridView", "ticketSize: $ticketSize , Rem: ${ticketSize.rem(2)}")
             layoutManager.spanSizeLookup = object : SpanSizeLookup() {
                 override fun getSpanSize(position: Int): Int {
-                  //  if ((position == 0 || position == 1) && ticketSize >1) return 2 else return 1
-
-                    if (ticketSize > 1 ){
-                        return when(position){
+                    return if (ticketSize > 1 ){
+                        when(position){
                             0,1 -> 1
                             else -> 2
                         }
-                    }else return 2
-
-                            /*
-                            return when(position){
-
-                                0,2 -> if (ticketSize > 1) 2 else 1
-                                1 -> 2
-                                2 -> 1
-                                else -> 1
-                            }
-                            */
-
-                    Log.d("ticketsGridView","Position: $position")
-                    //if (position == 0) return 2
-                    //else if (position == 1 && tickets.size == 2) return 2
-                   // else return 1
-
-                   // if (ticketSize % 2 == 0) return 2
-                    //else if (position ==0 || position ==2) return 1
-                    //else return 3
-                   // if (ticketSize % 2 == 0 && (position == 0 || position == 2)) return 1
-                    //else return 2
+                    }else 2
                 }
             }
 
             selectTicketFragmentView.pricesListRecyclerView.apply {
-
-
                 this.layoutManager = layoutManager
                 snapHelper.attachToRecyclerView(this)
                 adapter  = ticketsAdapter
