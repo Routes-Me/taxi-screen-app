060f000a1064469aa8d491627e1d22903d1098a4
abdullahsoubeih <asoubeih@routesme.com>
2021-05-10 16:56:07 +0300

    Refine the ticket buttons sizes.

--- app/src/bus/java/com/routesme/vehicles/view/fragment/TicketsFragment.kt
+++ app/src/bus/java/com/routesme/vehicles/view/fragment/TicketsFragment.kt
@@ -1,16 +1,20 @@
 package com.routesme.vehicles.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.routesme.vehicles.R
 import com.routesme.vehicles.uplevels.CarrierInformation
 import com.routesme.vehicles.view.adapter.PriceButtonRecyclerViewAdapter
 import kotlinx.android.synthetic.bus.fragment_tickets.view.*
+
 
 class TicketsFragment : Fragment(), PriceButtonRecyclerViewAdapter.ItemClickListener {
     private lateinit var selectTicketFragmentView: View
@@ -32,12 +36,55 @@ class TicketsFragment : Fragment(), PriceButtonRecyclerViewAdapter.ItemClickList
     }
 
     private fun setupPricesButtons() {
+
         val tickets = CarrierInformation().tickets
         tickets?.let { tickets ->
+            val snapHelper = LinearSnapHelper()
             ticketsAdapter = PriceButtonRecyclerViewAdapter(activity,tickets)
             ticketsAdapter.setClickListener(this)
+            val ticketSize = tickets.size
+            val layoutManager= GridLayoutManager(activity,2)
+
+            Log.d("ticketsGridView", "ticketSize: $ticketSize , Rem: ${ticketSize.rem(2)}")
+            layoutManager.spanSizeLookup = object : SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                  //  if ((position == 0 || position == 1) && ticketSize >1) return 2 else return 1
+
+                    if (ticketSize > 1 ){
+                        return when(position){
+                            0,1 -> 1
+                            else -> 2
+                        }
+                    }else return 2
+
+                            /*
+                            return when(position){
+
+                                0,2 -> if (ticketSize > 1) 2 else 1
+                                1 -> 2
+                                2 -> 1
+                                else -> 1
+                            }
+                            */
+
+                    Log.d("ticketsGridView","Position: $position")
+                    //if (position == 0) return 2
+                    //else if (position == 1 && tickets.size == 2) return 2
+                   // else return 1
+
+                   // if (ticketSize % 2 == 0) return 2
+                    //else if (position ==0 || position ==2) return 1
+                    //else return 3
+                   // if (ticketSize % 2 == 0 && (position == 0 || position == 2)) return 1
+                    //else return 2
+                }
+            }
+
             selectTicketFragmentView.pricesListRecyclerView.apply {
-                layoutManager = GridLayoutManager(activity,2)
+
+
+                this.layoutManager = layoutManager
+                snapHelper.attachToRecyclerView(this)
                 adapter  = ticketsAdapter
             }
         }
