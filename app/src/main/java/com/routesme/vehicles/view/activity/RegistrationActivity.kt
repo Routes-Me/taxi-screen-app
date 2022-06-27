4cfda57de609acadbbf413ae33007cb8ee5a2655
abdullahsoubeih <asoubeih@routesme.com>
2022-06-27 13:57:58 +0200

    Some refinement for integration steps of E60Q SDK 2

--- app/src/main/java/com/routesme/vehicles/view/activity/RegistrationActivity.kt
+++ app/src/main/java/com/routesme/vehicles/view/activity/RegistrationActivity.kt
@@ -135,8 +135,8 @@ class RegistrationActivity : AppCompatActivity(), View.OnClickListener {
             return
         } else {
             registerCredentials.apply {
-                serialNumber = telephonyManager.imei
-                SimSerialNumber = telephonyManager.simSerialNumber
+                serialNumber = "imei3333"//telephonyManager.imei
+                SimSerialNumber = "simSerialNumber3333"//telephonyManager.simSerialNumber
                 deviceId_tv.text = serialNumber
                 SimSerialNumber_tv.text = SimSerialNumber
             }
